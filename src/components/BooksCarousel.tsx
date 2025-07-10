863df0624ad405a66efa1ca2446df716ac459efe
Vitor Moscoso <vitormoscosos@gmail.com>
2025-07-10 20:59:04 -0300

    Refactor BooksCarousel component for improved layout and styling

--- src/components/BooksCarousel.tsx
+++ src/components/BooksCarousel.tsx
@@ -12,19 +12,19 @@ export function BooksCarousel() {
       <Carousel opts={{ align: "start" }} className="w-full">
         <CarouselContent>
           {Array.from({ length: 10 }).map((_, index) => (
-            <CarouselItem key={index} className="md:basis-1/4 lg:basis-1/4">
-              <div className="p-1 bg-[#3A6EA5]">
+            <CarouselItem key={index} className="basis-1/4 flex justify-center">
+              <div className="inline-block p-1 bg-[#3A6EA5]">
                 <img
-                  src={`https://covers.openlibrary.org/b/id/12818862-L.jpg`}
+                  src={`https://covers.openlibrary.org/b/id/14935910-L.jpg`}
                   alt={`Book ${index + 1}`}
-                  className="w-full h-auto object-cover"
+                  className="max-h-[250px] object-contain"
                 />
               </div>
             </CarouselItem>
           ))}
         </CarouselContent>
-        <CarouselPrevious className="left-2"/>
-        <CarouselNext className="right-2"/>
+        <CarouselPrevious className="left-2 bg-[#3A6EA5] text-white cursor-pointer" />
+        <CarouselNext className="right-2 bg-[#3A6EA5] text-white cursor-pointer" />
       </Carousel>
     </div>
   );
