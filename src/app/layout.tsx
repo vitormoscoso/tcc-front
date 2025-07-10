a7d73c22d569b3e2cc129ddc5267e9ccb0de83ab
Vitor Moscoso <vitormoscosos@gmail.com>
2025-07-10 20:01:19 -0300

    Refactor layout.tsx for improved readability and consistency

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,26 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
-import { cn } from '@/lib/utils';
+import { Header } from "@/components/Header";
+import { cn } from "@/lib/utils";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'My App',
-  description: 'Frontend usando NextJS, Firebase e Tailwind',
-}
+  title: "My App",
+  description: "Frontend usando NextJS, Firebase e Tailwind",
+};
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="pt-br">
-      <body className={cn(inter.className, "bg-gray-50 min-h-screen")}>{children}</body>
+      <body className={cn(inter.className, "bg-gray-50 min-h-screen")}>
+        <Header />
+        <main className="p-6">{children}</main>
+      </body>
     </html>
-  )
+  );
 }
