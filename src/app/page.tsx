3c044b9f405830ed43d40c3270b16bfdd9de92b3
Vitor Moscoso <vitormoscosos@gmail.com>
2025-07-10 20:42:17 -0300

    Add BooksCarousel component for displaying book covers in a carousel format

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,28 @@
+import { BooksCarousel } from "@/components/BooksCarousel";
+
 export default function HomePage() {
   return (
     <main className="p-6">
-      <h1 className="text-2xl font-bold">Página Inicial</h1>
+      <div className="w-full">
+        <div>
+          <h3 className="text-2xl font-bold mb-4 text-[#3A6EA5]">
+            Best Sellers
+          </h3>
+          <BooksCarousel />
+        </div>
+        <div>
+          <h3 className="text-2xl font-bold mb-2 mt-6 text-[#3A6EA5]">
+            Romance
+          </h3>
+          <BooksCarousel />
+        </div>
+        <div className="mt-4">
+          <h3 className="text-2xl font-bold mb-2 text-[#3A6EA5]">
+            Ficção Científica
+          </h3>
+          <BooksCarousel />
+        </div>
+      </div>
     </main>
   );
 }
---
+++ src/components/BooksCarousel.tsx
@@ -0,0 +1,31 @@
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+
+export function BooksCarousel() {
+  return (
+    <div className="relative w-full overflow-hidden">
+      <Carousel opts={{ align: "start" }} className="w-full">
+        <CarouselContent>
+          {Array.from({ length: 10 }).map((_, index) => (
+            <CarouselItem key={index} className="md:basis-1/4 lg:basis-1/4">
+              <div className="p-1 bg-[#3A6EA5]">
+                <img
+                  src={`https://covers.openlibrary.org/b/id/12818862-L.jpg`}
+                  alt={`Book ${index + 1}`}
+                  className="w-full h-auto object-cover"
+                />
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious className="left-2"/>
+        <CarouselNext className="right-2"/>
+      </Carousel>
+    </div>
+  );
+}
