344b24f4e7ee98935e9b369fd17f2f689baa5ce0
Vitor Moscoso <vitormoscoso@gmail.com>
2025-07-15 20:21:34 -0300

    add book details and review endpoints

--- src/services/books/bookService.ts
+++ src/services/books/bookService.ts
@@ -11,3 +11,13 @@ export const searchBooks = async (query: string) => {
   const res = await api.get(`/books?q=${encodeURIComponent(query)}`);
   return res.data;
 };
+
+export const getBookDetails = async (bookID: string) => {
+  const res = await api.get(`/books/${encodeURIComponent(bookID)}`);
+  return res.data
+}
+
+export const getBookReviews = async (bookID: string) => {
+  const res = await api.get(`/books/${encodeURIComponent(bookID)}/reviews`);
+  return res.data
+}
