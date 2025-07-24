2afed5624a14073c4e252efad76aa557903011f2
Vitor Moscoso <vitormoscosos@gmail.com>
2025-07-23 22:13:15 -0300

    Add checkIfBookIsInList function

--- src/services/lists/listService.ts
+++ src/services/lists/listService.ts
@@ -3,4 +3,13 @@ import api from "../api";
 export const addBookToList = async (review: any) => {
   const res = await api.post(`/lists/`, review);
   return res.data;
-}
+};
+
+export const checkIfBookIsInList = async (uid_firebase: string, id: string) => {
+  const res = await api.get(
+    `/lists/check?uid_firebase=${encodeURIComponent(
+      uid_firebase
+    )}&id=${encodeURIComponent(id)}`
+  );
+  return res.data;
+};
