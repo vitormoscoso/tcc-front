d212b7942651e9ea5b47c11cbbb01c7d32956c8e
Vitor Moscoso <vitormoscosos@gmail.com>
2025-07-26 11:01:55 -0300

    Update metadata title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "My App",
-  description: "Frontend usando NextJS, Firebase e Tailwind",
+  title: "Clube do Livro",
 };
 
 export default function RootLayout({
