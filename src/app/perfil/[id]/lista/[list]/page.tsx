7a637d0aaf66ed2aee6c84fdc9c7042f06e6c9cf
Vitor Moscoso <vitormoscosos@gmail.com>
2025-07-27 21:12:27 -0300

    Add ListPage component to display book lists and fetch data by user ID and list type

---
+++ src/app/perfil/[id]/lista/[list]/page.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { getList } from "@/services/lists/listService";
+import { BookDetails } from "@/types/book";
+import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export default function ListPage() {
+  const { id, list } = useParams();
+
+  const [listData, setListData] = useState<BookDetails[] | null>(null);
+
+  useEffect(() => {
+    const fetchListData = async () => {
+      try {
+        const response = await getList(id as string, list as string);
+        console.log("List Data:", response);
+        setListData(response);
+      } catch (error) {
+        console.error("Error fetching list data:", error);
+      }
+    };
+
+    fetchListData();
+  }, [id, list]);
+
+  return (
+    <div className="pt-10 px-26">
+      <div className="bg-[#3A6EA5] text-white rounded-lg p-6 flex flex-wrap gap-8">
+        {listData?.map((book) => (
+          <div
+            key={book?.id}
+            className="flex items-start gap-4"
+          >
+            {/* Capa */}
+            <img
+              src={book?.coverUrl}
+              alt={book?.title}
+              className="w-24 h-36 object-cover rounded-sm"
+            />
+
+            {/* Conteúdo */}
+            <div className="flex flex-col justify-between flex-1 space-y-2">
+              <div>
+                <h2 className="text-lg font-bold">{book?.title}</h2>
+                <p className="text-sm">
+                  {book?.authors?.length > 0
+                    ? book?.authors.map((a) => a.name).join(", ")
+                    : "-"}
+                </p>
+              </div>
+
+              <div className="flex items-center gap-1 text-sm">
+                <span>{book?.publishDate || "-"}</span>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
--- src/app/perfil/[id]/page.tsx
+++ src/app/perfil/[id]/page.tsx
@@ -4,10 +4,8 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Separator } from "@/components/ui/separator";
 import { useAuth } from "@/hooks/useAuth";
 import { BookOpen, User, Clock, Heart, Star } from "lucide-react";
-// import { useParams } from "next/navigation";
 
 export default function ProfilePage() {
-//   const { id } = useParams();
   const { user } = useAuth();
 
   return (
@@ -25,17 +23,32 @@ export default function ProfilePage() {
 
       <div className="w-[35rem] h-[20rem] flex items-center justify-center flex-col gap-4 mt-6 bg-[#3A6EA5] p-10 rounded-sm">
         <div className="bg-white text-[#3A6EA5] rounded-sm p-4 w-full h-full flex flex-col gap-2">
-          <div className="flex items-center gap-4 cursor-pointer">
+          <div
+            className="flex items-center gap-4 cursor-pointer"
+            onClick={() =>
+              (window.location.href = `/perfil/${user?.uid}/lista/favoritos`)
+            }
+          >
             <Heart color="#3A6EA5" />
             Livros favoritos
           </div>
           <Separator className="my-2" color="#3A6EA5" />
-          <div className="flex items-center gap-4 cursor-pointer">
+          <div
+            className="flex items-center gap-4 cursor-pointer"
+            onClick={() =>
+              (window.location.href = `/perfil/${user?.uid}/lista/avaliados`)
+            }
+          >
             <Star color="#3A6EA5" />
             Livros avaliados
           </div>
           <Separator className="my-2" color="#3A6EA5" />
-          <div className="flex items-center gap-4 cursor-pointer">
+          <div
+            className="flex items-center gap-4 cursor-pointer"
+            onClick={() =>
+              (window.location.href = `/perfil/${user?.uid}/lista/para_ler`)
+            }
+          >
             <Clock color="#3A6EA5" />
             Livros para ler depois
           </div>
